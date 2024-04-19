56e19fcdb110d38aff1d617faeb6e80082b94e17
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-04-19 16:53:29 -0400

    Finished the capitalize function and passed all test scenarios

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+// const array= require('../src/analyseArray.js');
+const capitalize = require('../src/capitalize.js');
+// const reverseString = require('../src/reverseString.js');
+// const calculator = require('../src/calculator.js');
+// const caesarCipher = require('../src/caesarCipher.js');
+
+module.exports = {
+    capitalize
+};
